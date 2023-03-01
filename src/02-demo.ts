57be69d3719f6038b6d0246e044cb5ffab87245a
Dtheran <dtheran1@gmail.com>
2023-03-01 14:07:41 -0500

    wip

---
+++ src/02-demo.ts
@@ -0,0 +1,6 @@
+const numbers = [1,2,3,4,5]
+
+console.log(numbers)
+
+const productPrice: number = 12
+
---
+++ src/03-typing.ts
@@ -0,0 +1,10 @@
+let myProductName= 'product 1'
+
+let myProductPrice = 123
+
+myProductName.toLowerCase()
+
+let hex = 0xfff
+let bin = 0b1010
+
+console.log(myProductName);
